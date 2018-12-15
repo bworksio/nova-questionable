68ddf7e7b9be6ba8cd470726d0168803ebcc3232
George Tsenis <g.tsenis@gmail.com>
2018-12-15 16:59:05 +0100

    Update

---
+++ README.md
@@ -0,0 +1,6 @@
+# Nova Questionable Plugin
+
+
+# Questionable Plugin
+php artisan vendor:publish --tag=questions-manager-migrations --force
+php artisan migrate
--- src/ToolServiceProvider.php
+++ src/ToolServiceProvider.php
@@ -19,11 +19,11 @@ class ToolServiceProvider extends ServiceProvider
     {
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'nova-questionable');
 
-        if (!class_exists('CreateQrcodesTables')) {
+        if (!class_exists('CreateQuestionsTables')) {
             $timestamp = date('Y_m_d_His', time());
             $this->publishes([
-                __DIR__ . '/../database/migrations/create_qrcodes_table.php.stub' => $this->app->databasePath() . "/migrations/{$timestamp}_create_qrcodes_table.php",
-            ], 'qrcode-manager-migrations');
+                __DIR__ . '/../database/migrations/create_questions_table.php.stub' => $this->app->databasePath() . "/migrations/{$timestamp}_create_questions_table.php",
+            ], 'questions-manager-migrations');
         }
 
         $this->app->booted(function () {
